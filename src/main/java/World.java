527d8baeab99db6a3c3ae3d061a110b7151088e3
ketaki.sharma <ketaki.sharma@metoffice.gov.uk>
2018-02-08 13:42:31 +0000

    WIP- Added Grid Creation & a test for phase2 pattern

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,5 +1,6 @@
 // TODO Package?
 
+
 public class World {
 
     public int[][] grid;
@@ -7,23 +8,51 @@ public class World {
     public int[][] createCurrentGrid(String currentStringGrid) {
 
         String[] gridRows = currentStringGrid.split(",");
-
+        grid = new int[gridRows.length][gridRows[0].length()];
         int row = 0;
         for(String currentRow :gridRows){
             for(int position=0; position<currentRow.length(); position++){
-                grid[row][position] = (int) currentRow.charAt(position);
+                char x =  currentRow.charAt(position);
+                int a = Character.getNumericValue(x);
+                grid[row][position] = a;
             }
             row++;
         }
         return grid;
     }
 
-// "0000010000" -> [0,0,0,1,0,0,0,0,0]
+    public int[][] changeGridState(int[][] currentGrid){
 
-// Grid's current
+
+        for(int r=0; r<11; r++) {
+            for (int p = 0; p < 15; p++) {
+
+                int living = 0;
+
+                living = grid[r-1][p-1] + grid[r-1][p] + grid[r-1][p+1]+
+                        grid[r][p-1] +                    grid[r][p+1] +
+                        grid[r+1][p-1] + grid[r+1][p] + grid[r+1][p+1] ;
+
+                if(grid[r][p] == 0 && living == 3){
+                        grid[r][p] = 1;
+                }else {
+                    if(living < 2 || living > 3){
+                        grid[r][p] = 0;
+                    }
+                }
+
+            }
+        }
+
+
+        return grid;
+    }
+
+
+// TODO Grid's current
 //   { [0,0,0,0],[0,0,1,0],[0,0,0,0],[0,0,0,0,]}
 
-// grids next state
+// TODO grids next state
 //   { [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0,]}
 
 }
